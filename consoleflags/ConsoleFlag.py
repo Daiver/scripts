f40a6c689d15e54d56e45c892a4e048ab65266b1
Klimov Kirill <ra22341@ya.ru>
2012-07-07 20:27:36 +0400

    first classes for consoleflags

---
+++ consoleflags/ConsoleFlag.py
@@ -0,0 +1,7 @@
+
+#POD, describes flag
+class ConsoleFlag:
+    def __init__(self, name, params):
+        self.name = name
+        self.params = params
+        
---
+++ consoleflags/FlagReader.py
@@ -0,0 +1,12 @@
+
+import ConsoleFlag
+
+#Reads flags and mksome actions with flags
+class FlagReader:
+
+    def __init__(self, flist):
+        self.flags = {}
+        self.loadfromlist(flist)
+
+    def loadfromlist(self, flist):
+        pass
