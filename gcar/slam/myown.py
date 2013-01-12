1431d27ac7ee19f41e4995bc1959bb6122f71532
kirill klimov <ra22341@ya.ru>
2013-01-12 13:05:27 +0400

    add making data

--- gcar/slam/myown.py
+++ gcar/slam/myown.py
@@ -10,7 +10,7 @@ landmarks = [
 
 world_size = 200
 
-class Agent(object):
+class Robot(object):
     def __init__(self, x, y, sense_power):
         self.x = x
         self.y = y
@@ -21,18 +21,38 @@ class Agent(object):
         for i, l in enumerate(landmarks):
             #dist = np.sqrt( (self.x - l[0])**2 + (self.y - l[1])**2 )
             dx = l[0] - self.x
-            dy = l[0] - self.y
+            dy = l[1] - self.y
             if (abs(dx) + abs(dy)) <= self.sense_power:
                 res.append([i, dx, dy])
         return res
 
-def showall(landmarks, waiting=0):
+    def simple_move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+def make_data(steps, landmarks, robot):
+    data = []
+    for st in steps:
+        Z = robot.sense()
+        robot.simple_move(st[0], st[1])
+        data.append([Z, st[0], st[1]])
+        showall(landmarks, robot, 300)
+    return data
+
+def showall(landmarks, robot, waiting=0):
     tmp = np.zeros((world_size, world_size, 3))
     tmp[:] = 255
     for l in landmarks:
         cv2.circle(tmp, (int(l[0]), int(l[1])), 3, (255, 0, 0))
+    cv2.circle(tmp, (int(robot.x), int(robot.y)), 3, (0, 255, 0))
     cv2.imshow('', tmp)
     cv2.waitKey(waiting)
 
 if __name__ == '__main__':
-    showall(landmarks)
+    robot = Robot(30., 30., 20)
+    path = [[1., 1.], [1., 1.], [1., 1.], [1., 1.],] 
+    for i in xrange(20):
+        path.append([0.0, 5.0])
+    print (make_data(path, landmarks, robot))
+
+    showall(landmarks, robot)
