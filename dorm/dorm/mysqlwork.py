46263d01f58cac4ef130d84694b34e45c28a972a
kirill klimov <ra22341@ya.ru>
2013-01-20 18:56:27 +0400

    useless

--- dorm/dorm/mysqlwork.py
+++ dorm/dorm/mysqlwork.py
@@ -1,19 +1,15 @@
 #!/usr/bin/ python
 # -*- coding: utf-8
-
 import MySQLdb
-
-import string
-
 import datetime
 
-
 class MySQLWork:
+    '''Small class for simple work with db '''
     def __init__(self,  host, user, passwd, db):
         self.db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, charset='utf8')
         self.cursor = self.db.cursor()
         self.log = {}
-    
+
     def __del__(self):
         try:
             self.Close()
@@ -24,13 +20,14 @@ class MySQLWork:
         self.db.commit()
         self.db.close()
     
-    def CountOfQuery():
+    def CountOfQuery(self):
         return len(self.log)
     
+    def Commit(self):
+        self.db.commit()
+
     def Run(self,str):
         self.cursor.execute(str.decode('utf8'))
         date = datetime.datetime.now()
         self.log[date] = str
-        return self.cursor.fetchall()        
-        
-        
+        return self.cursor.fetchall()    
