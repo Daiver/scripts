92b533f05d60134ca06e2a43475e4b8de25f22c8
kirill klimov <ra22341@ya.ru>
2013-01-21 23:04:12 +0400

    just for rec

--- uber/core/plugin.py
+++
@@ -1,9 +0,0 @@
-
-from component import Component
-
-class ComponentManager(Component):
-    def __init__(self):
-        super(ComponentManager, self).__init__()
-
-    def Register(self, componentmanager):
-        pass
