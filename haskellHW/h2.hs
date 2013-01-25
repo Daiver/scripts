4fa5a4122da87eef06282a996e7b859383f2f30f
Daiver <ra22341@ya.ru>
2013-01-25 11:32:25 +0400

    minor changes

--- haskellHW/h2.hs
+++ haskellHW/h2.hs
@@ -1,22 +1,28 @@
+--infinity lists
 numesfrom n = n : numesfrom (n + 1)
 natural = numesfrom 0
 squares n = map (^2) (numesfrom n)
+
+--define sub function
 deepfun x y = f1 x + f2 y
     where 
         f1 x = x^2
         f2 x = 2 * f21 x
             where f21 x = x-1
-
+--condition ????
 condfunc1 x y 
         | x > y = "X > Y"
         | x < y = "X < Y"
         | x == y = "X == Y"
 
+--listwork
 somelist1 = [1, 2, 3, 8798966]
 psomelist1 = tail somelist1
 psomelist2 = head somelist1
 psomelist3 = last somelist1
 psomelist4 = init somelist1
+
+
 main = do--putStrLn 34
     print psomelist4
     --print (take 9 (squares 0))
