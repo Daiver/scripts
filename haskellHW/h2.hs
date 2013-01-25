7d1fe061957edcef057e46892d382a832d0b9acd
Daiver <ra22341@ya.ru>
2013-01-25 11:48:12 +0400

    ;)

--- haskellHW/h2.hs
+++ haskellHW/h2.hs
@@ -37,7 +37,13 @@ rangesample1 = ['a' .. 'd']
 condlist = [x**2 | x <- [-2..10], x > 2]
 --from other resurse
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
+--something amazing
+condlist2 = [ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50]
+--arrrrrr my mind
+nouns = ["hobo","frog","pope"] 
+adjectives = ["lazy","grouchy","scheming"]  
+condlist3 = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]  
 
 main = do--putStrLn 34
-    print (boomBangs [1..20])
+    print condlist3
     --print (take 9 (squares 0))
