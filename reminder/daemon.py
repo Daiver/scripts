1bdf9d74e36c748e62e11b92e885123cedfdcf4f
Daiver <ra22341@ya.ru>
2013-01-27 14:41:47 +0400

    trying to start daemon

--- reminder/daemon.py
+++ reminder/daemon.py
@@ -2,20 +2,28 @@
 
 import pynotify
 import time
+import datetime
+from notimodel import Notification, get_sesnmetneng
 
 class NotifyDaemon(object):
-    def __init__(self):
-        self.index = 0
+    def __init__(self, dbses):
+        self.dbses = dbses
+        self.timeout = 5
 
     def run(self):
         while True:
-            self.index += 1
-            n = pynotify.Notification('Test', 'text ' + str(self.index))
-            n.set_timeout(10000)
-            n.show()
-            time.sleep(3)
+            now = datetime.datetime.now()
+            cand_notes = self.dbses.query(Notification).filter(Notification.dt < now).all()
+            for note in cand_notes:
+                if not note.has_shown:
+                    n = pynotify.Notification('NOTIFICATION\n%s' % note.title, '%s\nDate:%s' % ( note.text, str(note.dt)))
+                    n.set_timeout(10000)
+                    n.show()
+                    note.has_shown = True
+            self.dbses.commit()
+            time.sleep(self.timeout)
 
 if __name__ == '__main__':
     pynotify.init('reminder')
-    daemon = NotifyDaemon()
+    daemon = NotifyDaemon(get_sesnmetneng('reminder.db')[0])
     daemon.run()
