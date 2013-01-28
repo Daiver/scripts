22543b49a88c8f8de8f5ee8b5b93cc5ea41e7ee0
Daiver <ra22341@ya.ru>
2013-01-28 20:20:16 +0400

    minor changes

--- cppexp/filework/main.cpp
+++ cppexp/filework/main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 #include<stdio.h>
+#include<fstream>
 
 int main(int argc, char** argv)
 {
     printf("Start...\n");
+    std::ofstream out("myfile.txt"); // Открываем файл для записи
+    out<<12.012<<" "<<16.12<<"\n"; //Записываем первую строчку
+    out<<"UraRabotaet!!!"; //Записываем вторую строчку
+    out.close(); //Закрываем файл
     return 0;
 }
