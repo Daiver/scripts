73885300ced00c7c92398285895eeab1f1ae647d
Daiver <ra22341@ya.ru>
2013-01-29 08:13:52 +0400

    somw

--- cppexp/filework/main.cpp
+++ cppexp/filework/main.cpp
@@ -6,8 +6,12 @@ int main(int argc, char** argv)
 {
     printf("Start...\n");
     std::ofstream out("myfile.txt"); // Открываем файл для записи
-    out<<12.012<<" "<<16.12<<"\n"; //Записываем первую строчку
-    out<<"UraRabotaet!!!"; //Записываем вторую строчку
+    for (int i = 0; i < 1000000; i++)
+    {
+        out<<12.012<<" "<<16.12<<"\n"; //Записываем первую строчку
+        out<<"UraRabotaet!!!"; //Записываем вторую строчку
+        out.flush();
+    }
     out.close(); //Закрываем файл
     return 0;
 }
