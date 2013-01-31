0ba79235daec19ce3255a5a358f9a04b5803e883
Daiver <ra22341@ya.ru>
2013-01-31 17:59:35 +0400

    minor work with send

--- cppexp/sharedobjects/recv.cpp
+++ cppexp/sharedobjects/recv.cpp
@@ -5,22 +5,16 @@
 #include<stdio.h>
 #include <cstdlib> //std::system
 
-using namespace boost::interprocess;
-typedef allocator<float, managed_shared_memory::segment_manager>  ShmemAllocator;
-typedef vector<float, ShmemAllocator> DataVector;
+//using namespace boost::interprocess;
+typedef boost::interprocess::allocator<float, boost::interprocess::managed_shared_memory::segment_manager>  ShmemAllocator;
+typedef boost::interprocess::vector<float, ShmemAllocator> DataVector;
 int main(int argc, char *argv[])
 {
-      printf("Start...\n");
-      managed_shared_memory segment(open_only, "MySharedMemory");
-      printf("Start...\n");
+      boost::interprocess::managed_shared_memory segment(boost::interprocess::open_only, "MySharedMemory");
       DataVector *myvector = segment.find<DataVector>("MyVector").first;
-      printf("Start...\n");
-      //std::sort(myvector->rbegin(), myvector->rend());
       for(int i = 0; i < 100; ++i)  //Insert data in the vector
       {
         printf("%f ", (float)myvector->at(i));
       }
-      //segment.destroy<DataVector>("MyVector");
-
    return 0;
 };
