122c54a7fac79702c7f12f3948a7312c21541682
Daiver <ra22341@ya.ru>
2013-01-31 17:57:07 +0400

    working shm

--- cppexp/sharedobjects/recv.cpp
+++ cppexp/sharedobjects/recv.cpp
@@ -10,41 +10,17 @@ typedef allocator<float, managed_shared_memory::segment_manager>  ShmemAllocator
 typedef vector<float, ShmemAllocator> DataVector;
 int main(int argc, char *argv[])
 {
-/*   if(argc == 1){ 
-      struct shm_remove
-      {
-         shm_remove() { shared_memory_object::remove("MySharedMemory"); }
-         ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
-      } remover;
-
-      managed_shared_memory segment(create_only, "MySharedMemory", 65536);
-
-      const ShmemAllocator alloc_inst (segment.get_segment_manager());
-
-      DataVector *myvector = segment.construct<DataVector>("MyVector")(alloc_inst);
-
-      for(int i = 0; i < 100; ++i)  //Insert data in the vector
-         myvector->push_back(i);
-
-      std::string s(argv[0]); s += " child ";
-      if(0 != std::system(s.c_str()))
-         return 1;
-
-      if(segment.find<DataVector>("MyVector").first)
-         return 1;
-   }*/
-   if (1) { 
       printf("Start...\n");
       managed_shared_memory segment(open_only, "MySharedMemory");
       printf("Start...\n");
       DataVector *myvector = segment.find<DataVector>("MyVector").first;
-      std::sort(myvector->rbegin(), myvector->rend());
+      printf("Start...\n");
+      //std::sort(myvector->rbegin(), myvector->rend());
       for(int i = 0; i < 100; ++i)  //Insert data in the vector
       {
         printf("%f ", (float)myvector->at(i));
       }
-      segment.destroy<DataVector>("MyVector");
-   }
+      //segment.destroy<DataVector>("MyVector");
 
    return 0;
 };
