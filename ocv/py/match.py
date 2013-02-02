9dd3da76809e9be023dc3e4ace6a5c5bb3fa3373
Daiver <ra22341@ya.ru>
2013-02-02 20:52:41 +0400

    minor changes

--- ocv/py/match.py
+++ ocv/py/match.py
@@ -13,16 +13,6 @@ FLANN_INDEX_KDTREE = 1  # bug: flann enums are missing
 flann_params = dict(algorithm = FLANN_INDEX_KDTREE,
                     trees = 4)
 
-def match_bruteforce(desc1, desc2, r_threshold = 0.75):
-    res = []
-    for i in xrange(len(desc1)):
-        dist = anorm( desc2 - desc1[i] )
-        n1, n2 = dist.argsort()[:2]
-        r = dist[n1] / dist[n2]
-        if r < r_threshold:
-            res.append((i, n1))
-    return np.array(res)
-
 def match_flann(desc1, desc2, r_threshold = 0.6):
     flann = cv2.flann_Index(desc2, flann_params)
     idx2, dist = flann.knnSearch(desc1, 2, params = {}) # bug: need to provide empty dict
@@ -88,14 +78,13 @@ if __name__ == '__main__':
         matched_p2 = np.array([kp2[j].pt for i, j in m])
         H, status = cv2.findHomography(matched_p1, matched_p2, cv2.RANSAC, 5.0)
         print '%d / %d  inliers/matched' % (np.sum(status), len(status))
-
         vis = draw_match(img1, img2, matched_p1, matched_p2, status, H)
         return vis
 
     #print 'bruteforce match:',
     #vis_brute = match_and_draw( match_bruteforce, 0.75 )
     print 'flann match:',
-    vis_flann = match_and_draw( match_flann, 0.6 ) # flann tends to find more distant second
+    vis_flann = match_and_draw( match_flann, 0.79 ) # flann tends to find more distant second
                                                    # neighbours, so r_threshold is decreased
     #cv2.imshow('find_obj SURF', vis_brute)
     cv2.imshow('find_obj SURF flann', vis_flann)
