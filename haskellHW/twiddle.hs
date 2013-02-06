1f697e856c5631634a9b8209539753a629a46e47
Daiver <ra22341@ya.ru>
2013-02-06 21:17:28 +0400

    i can it compile but it is unworks and ugly

--- haskellHW/twiddle.hs
+++ haskellHW/twiddle.hs
@@ -1,15 +1,16 @@
 import Data.List
 replaceAtIndex n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
 enumerate = zip [0..]
-twiddle estimate p dp tol = twid estimate (zip p dp (estimate p)) tol (estimate p)
+twiddle estimate p dp tol = twid estimate (zip3 p dp [estimate p | _ <- p]) tol (estimate p)
     where 
-        twid estimate p tol err 
+        twid estimate pp tol err --pp - packed params
             | err < tol = p
-            | otherwise = map optimizeOne (enumerate p)
-            where optimizeOne (i, (par, dpar, err))
-                | estimate (replaceAtIndex i (par + dpar) p) < err = (par + dpar, dpar*1.1, estimate (replaceAtIndex i (par + dpar) p))
-                | estimate (replaceAtIndex i (par - dpar) p) < err = (par - dpar, dpar*1.1, estimate (replaceAtIndex i (par - dpar) p))
-                | otherwise = (par, dpar*0.9, err)
+            | otherwise = twid estimate (map optimizeOne (enumerate pp)) tol err
+            where 
+                optimizeOne (i, (par, dpar, err))
+                    | estimate (replaceAtIndex i (par + dpar) p) < err = (par + dpar, dpar*1.1, estimate (replaceAtIndex i (par + dpar) p))
+                    | estimate (replaceAtIndex i (par - dpar) p) < err = (par - dpar, dpar*1.1, estimate (replaceAtIndex i (par - dpar) p))
+                    | otherwise = (par, dpar*0.9, err)
 
 
 est l = 0.00001
