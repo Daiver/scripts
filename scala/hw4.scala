2e262999c0addb3d684b83694a2f0e1a23168240
Daiver <ra22341@ya.ru>
2013-02-08 18:57:33 +0400

    minor work

---
+++ scala/hw4.scala
@@ -0,0 +1,18 @@
+object Tmr
+{
+    def time_work(callback : () => Unit, timeout:Int) = {
+        while(true) {
+            callback()
+            Thread.sleep(timeout)
+        }
+    }
+
+    def test_callback() = {
+        println("Wow")
+    }
+
+    def main(args : Array[String]) = {
+        println("HW!")
+        time_work(test_callback, 1)
+    }
+}
