98f39038d7d7daafd09c204fc4dab8487f215c97
Daiver <ra22341@ya.ru>
2013-02-18 19:06:22 +0400

    first version

--- cpp2013/ultoa/main.cpp
+++ cpp2013/ultoa/main.cpp
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdio>
+#include <iostream>
+
+#include <sstream>
 
 typedef  char (*testfunc)(unsigned long value, char *string, int radix);
 
-void test_func(testfunc foo, unsigned long value, int radix, char *true_value)
+char ultoa_by_sprintf(unsigned long value, char *string, int radix)
 {
+    sprintf(string, "%ld\0", value);
+    return 0;
+}
 
+void assert_func(testfunc foo, unsigned long value, int radix, const char *true_value)
+{
+    char *source = new char[36];
+    foo(value, source, radix);
+    printf(">%s\n", source);
 }
 
 int main(int argc, char** argv)
 {
-    printf("Hello\n");
+    assert_func(ultoa_by_sprintf, 12, 10, "");
     return 0;
 }
