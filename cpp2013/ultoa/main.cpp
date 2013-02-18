86be374b8fa59f8f2bf338aaeb58ff6eba9a1e9a
Daiver <ra22341@ya.ru>
2013-02-18 18:44:57 +0400

    minor changes

--- cpp2013/ultoa/main.cpp
+++ cpp2013/ultoa/main.cpp
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef  char (*testfunc)(unsigned long value, char *string, int radix);
+
+void test_func(testfunc foo, unsigned long value, int radix, char *true_value)
+{
+
+}
 
 int main(int argc, char** argv)
 {
