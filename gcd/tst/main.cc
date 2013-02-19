54746d5e213bf28c6789b3128b4a8a24d936e230
Daiver <ra22341@ya.ru>
2013-02-19 23:57:23 +0400

    :(

--- gcd/tst/main.cc
+++ gcd/tst/main.cc
@@ -22,8 +22,24 @@ static void read_completion_handler(dispatch_data_t data,
                         NULL, write_completion_handler);
 }
 
+int do_work(int *data, int i)
+{
+    return data[i]*i;
+}
+
 int main(int argc, const char *argv[]) {
-    dispatch_source_t timer = dispatch_source_create(
+    int arr_size = 10;
+    int *data = new int[arr_size];
+    int *result = new int[arr_size];
+    int i;
+    for(int i = 0; i < arr_size; i++) data[i] = i;
+    dispatch_apply_f(arr_size, dispatch_get_global_queue(0, 0), (size_t i){
+             result[i] = do_work(data, i);
+         });
+    for(int i = 0; i < arr_size; i++) printf("%d ", result[i]);
+    
+    delete[] data;
+    /*dispatch_source_t timer = dispatch_source_create(
         DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
 
     dispatch_source_set_event_handler_f(timer, timer_did_fire);
@@ -36,6 +52,6 @@ int main(int argc, const char *argv[]) {
     dispatch_read_f_np(fd, 1024, dispatch_get_main_queue(), (void *)(intptr_t)fd,
                        read_completion_handler);
 
-    dispatch_main();
+    dispatch_main();*/
     return 0;
 }
