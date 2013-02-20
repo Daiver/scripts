67385f54ea9493f752eeed7d606b5b8c17cdaf34
Daiver <ra22341@ya.ru>
2013-02-20 10:21:44 +0400

    second test added

--- cpp2013/bank/test.cc
+++ cpp2013/bank/test.cc
@@ -37,6 +37,25 @@ bool acccount_balance_test(unsigned long num_of_iter)
     return true;
 }
 
+bool acccount_transfer_test()
+{
+    Account *ac1 = new Account();
+    Account *ac2 = new Account(100);
+    
+    if ((!ac2->transfer_money_to(ac1, 20)) || (ac2->get_Balance() != 80) || (ac1->get_Balance() != 20))
+    {
+        return false;
+    }
+    
+    if ((ac1->transfer_money_to(ac2, 30)) || (ac2->get_Balance() != 80) || (ac1->get_Balance() != 20))
+    {
+        return false;
+    }
+
+
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     //Account ac1;
@@ -44,6 +63,7 @@ int main(int argc, char** argv)
     //std::cout<< "so..." << ac1.get_Balance() << " " << ac2.get_Balance() << "\n";
 
     pass_test(acccount_balance_test(100), "acccount_balance_test");
+    pass_test(acccount_transfer_test(), "acccount_transfer_test");
 
     return 0;
 }
