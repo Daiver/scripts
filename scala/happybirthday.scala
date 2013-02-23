bb04badb711d0a1174833f0fe42f8f93432d1359
Daiver <ra22341@ya.ru>
2013-02-23 16:07:34 +0400

    scala twiddle (first)

--- scala/happybirthday.scala
+++ scala/happybirthday.scala
@@ -1,6 +1,6 @@
 object app {
     def main(args : Array[String]) = {
-        (1 to 4).map { i => "Happy Birthday " + (if (i == 3) "dear " + (if (args.length < 1) "Name" else args(0)) else "to You") }.map { println }
+        (1 to 4).map { i => "Happy Birthday " + (if (i == 3) "dear " + (if (args.length < 1) "Name" else args(0)) else "to You") }.map ( println )
     }
 }
 
