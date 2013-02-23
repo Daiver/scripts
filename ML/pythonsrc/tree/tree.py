af40f08dd9028442fb4f010a663fff2397752ca3
Daiver <ra22341@ya.ru>
2013-02-23 23:25:00 +0400

    minor work with trees

--- ML/pythonsrc/tree/tree.py
+++ ML/pythonsrc/tree/tree.py
@@ -5,6 +5,20 @@ class decisionnode:
         self.results = results
         self.tb = tb
         self.fb = fb
+
+def divideset(rows,column,value):
+    split_function=None
+    if isinstance(value,int) or isinstance(value,float):
+        split_function=lambda row:row[column]>=value
+    else:
+        split_function=lambda row:row[column]==value
+    set1=[row for row in rows if split_function(row)]
+    et2=[row for row in rows if not split_function(row)]
+    return (set1,set2)
+
+def make_data_valid(data):
+    return [[str(x) if i != 3 else float(x) for i, x in enumerate(row) ] for row in data]
+
 if __name__ == '__main__':
     data = [[ex for ex in s.split('\t')] for s in open('data')]
-    print(data)
+    print(make_data_valid(data))
