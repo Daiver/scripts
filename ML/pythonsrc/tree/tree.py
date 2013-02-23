fcef5f099779fdb3d4ecdd3ee77a97b4f8d0d5c4
Daiver <ra22341@ya.ru>
2013-02-23 23:54:34 +0400

    minor work with trees

--- ML/pythonsrc/tree/tree.py
+++ ML/pythonsrc/tree/tree.py
@@ -36,10 +36,21 @@ def giniimpurity(rows):
             imp+=p1*p2
     return imp
 
+def entropy(rows):
+    from math import log
+    log2=lambda x:log(x)/log(2)
+    results=uniquecounts(rows)
+    ent=0.0
+    for r in results.keys( ):
+        p=float(results[r])/len(rows)
+        ent=ent-p*log2(p)
+    return ent
+
 def make_data_valid(data):
     return [[str(x) if i != 3 else float(x) for i, x in enumerate(row) ] for row in data]
 
 if __name__ == '__main__':
     data = [[ex for ex in s.split('\t')] for s in open('data')]
-    print(divideset(make_data_valid(data), 2, 'yes')[0])
-    print(divideset(make_data_valid(data), 2, 'yes')[1])
+    #print(divideset(make_data_valid(data), 2, 'yes')[0])
+    #print(divideset(make_data_valid(data), 2, 'yes')[1])
+    print(entropy(make_data_valid(data)))
