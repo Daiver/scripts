add5fc66ebb45b811a8a736c18a63c5f0bb9e29c
Daiver <ra22341@ya.ru>
2013-02-23 23:49:34 +0400

    minor work with trees

--- ML/pythonsrc/tree/tree.py
+++ ML/pythonsrc/tree/tree.py
@@ -13,12 +13,33 @@ def divideset(rows,column,value):
     else:
         split_function=lambda row:row[column]==value
     set1=[row for row in rows if split_function(row)]
-    et2=[row for row in rows if not split_function(row)]
+    set2=[row for row in rows if not split_function(row)]
     return (set1,set2)
+
+def uniquecounts(rows):
+    results={}
+    for row in rows:
+        r=row[len(row)-1]
+        if r not in results: results[r]=0
+        results[r]+=1
+    return results
+
+def giniimpurity(rows):
+    total=len(rows)
+    counts=uniquecounts(rows)
+    imp=0
+    for k1 in counts:
+        p1=float(counts[k1])/total
+        for k2 in counts:
+            if k1==k2: continue
+            p2=float(counts[k2])/total
+            imp+=p1*p2
+    return imp
 
 def make_data_valid(data):
     return [[str(x) if i != 3 else float(x) for i, x in enumerate(row) ] for row in data]
 
 if __name__ == '__main__':
     data = [[ex for ex in s.split('\t')] for s in open('data')]
-    print(make_data_valid(data))
+    print(divideset(make_data_valid(data), 2, 'yes')[0])
+    print(divideset(make_data_valid(data), 2, 'yes')[1])
