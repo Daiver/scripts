f8c235768d3f11c174c686d31cbda8e11939a71e
Daiver <ra22341@ya.ru>
2013-02-23 16:53:48 +0400

    scala twiddle (fiveth)

--- scala/twiddle.scala
+++ scala/twiddle.scala
@@ -7,17 +7,37 @@ object App {
 
     def twiddle(foo : (Array[Float]) => Float, initial_params : Array[Float], tol : Float) = {
         var params = initial_params.clone()
-        var diff_params = Array.fill(initial_params.length){0.0f}
-        while (foo(params) > tol) {
-            for(i <- (0 to params.length))
+        var diff_params = Array.fill(initial_params.length){1.0f}
+        var best_error = foo(params)
+        while (foo(params).abs > tol) {
+            for(i <- (0 to params.length - 1))
             {
-                println(i)
+                params(i) += diff_params(i)
+                val error = foo(params)
+                if (error < best_error) {
+                    diff_params(i) *= 1.1f
+                    best_error = error
+                } else {
+                    params(i) -= 2*diff_params(i)
+                    val error = foo(params)
+                    if (error < best_error) {
+                        diff_params(i) *= 1.1f
+                        best_error = error
+                    } else {
+                        params(i) += diff_params(i)
+                        diff_params(i) *= 0.9f
+                    }
+
+                }
+
             }
         }
-        params.toList
+        params
     }
 
     def main (args : Array[String]) = {
-        println(twiddle(func, Array(0, 0, 0), 0.001f))
+        val params = twiddle(func, Array(1, 1, 1), 0.01f)
+        println(params.toList)
+        println(func(params))
     }
 }
