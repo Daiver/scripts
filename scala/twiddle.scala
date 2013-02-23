9487364873675d5b386e2f3ce894632f4a47166f
Daiver <ra22341@ya.ru>
2013-02-23 16:17:47 +0400

    scala twiddle (third)

--- scala/twiddle.scala
+++ scala/twiddle.scala
@@ -2,7 +2,7 @@
 object App {
     def twiddle(foo : (Array[Float]) => Float, initial_params : Array[Float], tol : Float) = {
         var params = initial_params.clone()
-        params(0) = 10.
+        params(0) += 10
     }
 
     def main (args : Array[String]) = {
