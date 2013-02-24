a92a39260cef59939d3bccd26cdebad3e2d3a7d9
Daiver <ra22341@ya.ru>
2013-02-24 20:59:24 +0400

    -

--- cpp2013/threadpool/ThreadPool.cpp
+++ cpp2013/threadpool/ThreadPool.cpp
@@ -3,7 +3,10 @@
 
 ThreadPool::ThreadPool()
 {
-    this->queue = dispatch_queue_create("com.mydomain.myapp.longrunningfunction", DISPATCH_QUEUE_CONCURRENT);
+    for(int i = 0; i < QUEUE_COUNT; i++)
+    {
+        this->queue[i] = dispatch_queue_create("com.mydomain.myapp.longrunningfunction", DISPATCH_QUEUE_CONCURRENT);
+    }
 }
 
 void run_Operation_async(dispatch_queue_t queue, Operation *op)
@@ -15,5 +18,5 @@ void run_Operation_async(dispatch_queue_t queue, Operation *op)
 
 void ThreadPool::async(Operation *op)
 {
-    run_Operation_async(this->queue, op);
+    run_Operation_async(this->queue[2], op);
 }
--- cpp2013/threadpool/ThreadPool.h
+++ cpp2013/threadpool/ThreadPool.h
@@ -3,13 +3,15 @@
 #include "Operation.h"
 #include <dispatch/dispatch.h>
 
+#define QUEUE_COUNT 5
+
 class ThreadPool
 {
 public:
     ThreadPool();
     void async(Operation *op);
 private:
-    dispatch_queue_t queue;
+    dispatch_queue_t queue[QUEUE_COUNT];
 };
 #else
 #endif
