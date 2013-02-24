1bf73005291397a3a1824b8e81e9f3c4a17cdfe9
Daiver <ra22341@ya.ru>
2013-02-24 17:34:04 +0400

    add enum

--- cpp2013/threadpool/Operation.h
+++ cpp2013/threadpool/Operation.h
@@ -1,5 +1,15 @@
 #ifndef __OPERATION_H__
 #define __OPERATION_H__
+
+enum Operation_Priority
+{
+    very_hight = 2,
+    hight = 1,
+    normal = 0,
+    low = -1,
+    very_low = -2
+};
+
 class Operation
 {
 public:
