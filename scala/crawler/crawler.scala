7189e95581a03c0986143e51e82223d0dc4b5435
Daiver <ra22341@ya.ru>
2013-02-26 13:58:22 +0400

    ...

--- scala/crawler/crawler.scala
+++ scala/crawler/crawler.scala
@@ -1,15 +1,31 @@
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
-import java.io.{File, FileOutputStream}
-import java.net.{URL, URLConnection}
-import java.util.regex.Pattern
-import java.util.Scanner
+import scala.xml.Elem
+import scala.xml.Node
+import scala.collection.mutable.Queue
+import scala.xml.Text
+import scala.xml.PrettyPrinter
 import scala.xml._
+import java.net.URL
+
+/*
+object Reader {
+    def loadXML = {
+        val parserFactory = new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+        val parser = parserFactory.newSAXParser()
+        val source = new org.xml.sax.InputSource("http://www.randomurl.com")
+        val adapter = new scala.xml.parsing.NoBindingFactoryAdapter      
+        val feed = adapter.loadXML(source, parser)
+        feed
+    }
+}
+*/
 
 object App {
     def main(args : Array[String]) = {
+
         println("!")
-        val url = new URL("http://friendfeed.com/api/feed/public?format=xml&num=100")
+        val url = new URL("http://google.com")
         val conn = url.openConnection
+        //println(conn.getInputStream())
         println(XML.load(conn.getInputStream))
     }
 }
