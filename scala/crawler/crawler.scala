e8a2c79318d4a607793b052193bfec3b34b901f9
Daiver <ra22341@ya.ru>
2013-02-26 14:36:35 +0400

    ...

--- scala/crawler/crawler.scala
+++ scala/crawler/crawler.scala
@@ -1,31 +1,41 @@
-import scala.xml.Elem
-import scala.xml.Node
-import scala.collection.mutable.Queue
-import scala.xml.Text
-import scala.xml.PrettyPrinter
-import scala.xml._
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
+import java.io.{File, FileOutputStream}
 import java.net.URL
-
-/*
-object Reader {
-    def loadXML = {
-        val parserFactory = new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
-        val parser = parserFactory.newSAXParser()
-        val source = new org.xml.sax.InputSource("http://www.randomurl.com")
-        val adapter = new scala.xml.parsing.NoBindingFactoryAdapter      
-        val feed = adapter.loadXML(source, parser)
-        feed
-    }
-}
-*/
+import java.util.regex.Pattern
+import java.util.Scanner
 
 object App {
+    def openResourceInputStream(url: String) = {
+        val connection = new URL(url).openConnection()
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible;)")
+        connection.connect()
+        connection.getInputStream
+    }
+
+    def grabPage(pageScanner : Scanner)= {
+        var res = ""
+        while (pageScanner.hasNextLine) {
+            res += pageScanner.nextLine()
+        }
+        res
+    }
+
+    def getHref(str : String) = {
+        //val imagePattern = Pattern.compile("(src|href)[^><\"]*\"([^\"\']*\\.(gif|jpg|png|html))\"")
+        val imagePattern = Pattern.compile("""http:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+""")
+        val matcher = imagePattern.matcher(str)
+        while(matcher.find()) {
+            println(matcher.group(0))
+        }
+    }
+
     def main(args : Array[String]) = {
 
-        println("!")
-        val url = new URL("http://google.com")
-        val conn = url.openConnection
+        val pageScanner = new Scanner(openResourceInputStream("http://habrahabr.ru"))
+        val page = grabPage(pageScanner)
+        println(getHref(page))
+
         //println(conn.getInputStream())
-        println(XML.load(conn.getInputStream))
+        //println(XML.load(conn.getInputStream))
     }
 }
