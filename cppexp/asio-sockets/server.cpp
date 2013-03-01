d7ccb85544b615a7aefbd048a29b94d615070361
Daiver <ra22341@ya.ru>
2013-03-01 21:31:02 +0400

    minor changes

--- cppexp/asio-sockets/server.cpp
+++ cppexp/asio-sockets/server.cpp
@@ -21,13 +21,23 @@ int main(int argc, char** argv)
         for (;;)
         {
             tcp::socket socket(io_service);      
-            std::cout<<"Accept\n";
+            //std::cout<<"Accept\n";
             acceptor.accept(socket);
             std::cout<<"Accept\n";
-            std::string message = make_daytime_string();
-
-            boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+            for(;;)
+            {
+                boost::array<char, 128> buf;
+                boost::system::error_code error;
+                size_t len = socket.read_some(boost::asio::buffer(buf), error);
+                if (error == boost::asio::error::eof)
+                    break; // Connection closed cleanly by peer.
+                else if (error)
+                    throw boost::system::system_error(error); // Some other error.
+                std::cout.write(buf.data(), len);
+            }
+            //std::string message = make_daytime_string();
+            //boost::system::error_code ignored_error;
+            //boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
         }
     }
     catch (std::exception& e)
