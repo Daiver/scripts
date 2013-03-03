91a9a6bcd001b3a623469068a05eea334f41a43a
Daiver <ra22341@ya.ru>
2013-03-03 23:34:13 +0400

    some tests

--- cpp2013/threadpool/main.cc
+++ cpp2013/threadpool/main.cc
@@ -7,27 +7,61 @@
 #include "ThreadPool.h"
 
 
-void foo(void (^block)(void))
+void exec_block(void (^block)(void))//del it
 {
     void (^f)(void);
     f = block;
     f();
 }
 
+int tmp_data = 0;
+
+void foo(void *data)
+{
+    printf("foo! %d\n", (*(int*)data));
+    //std::cout<<"foo2"<<std::endl<<std::cout.flush();
+}
+
 void foo2(void *data)
 {
     printf("foo2!\n");
-    //std::cout<<"foo2"<<std::endl<<std::cout.flush();
 }
 
 void foo3(void *data)
 {
-    printf("foo3!\n");
+    printf("foo3\n");
 }
 
 void foo4(void *data)
 {
     printf("foo4\n");
+}
+
+int main(int argc, char** argv)
+{
+    ThreadPool pool(3);
+    int tst = 100;
+    FunctionOperation op1(foo, &tst);
+    FunctionOperation op2(foo2, NULL);
+    FunctionOperation op3(foo3, NULL);
+    FunctionOperation op4(foo4, NULL);
+    op1.set_priority(low);
+    //pool.async(&op2);
+    //pool.async(&op2);
+    //pool.async(&op1);
+    //pool.async(&op2);
+    op4.add_dependency(&op1);
+    op3.add_dependency(&op2);
+    op4.add_dependency(&op3);
+    pool.async(&op4);
+
+    //run_Operation_async(queue, &op2);
+    //dispatch_group_t group = dispatch_group_create()
+    //foo(^(void){ printf("!!!!!!\n");});
+    sleep(1);
+    //tmpfoo();
+    std::cout<<"THE END";
+    return 0;
 }
 
 void tmpfoo()
@@ -49,29 +83,4 @@ void tmpfoo()
     dispatch_group_wait(gr,  DISPATCH_TIME_FOREVER);
     dispatch_release(queue);
     printf("End tmp foo\n");
-}
-int main(int argc, char** argv)
-{
-    ThreadPool pool(3);
-    int tst = 100;
-    FunctionOperation op2(foo2, &tst);
-    FunctionOperation op(foo3, NULL);
-    op.add_dependency(&op2);
-    op.set_priority(low);
-    //pool.async(&op2);
-    //pool.async(&op2);
-    //pool.async(&op);
-    //pool.async(&op2);
-    FunctionOperation op3(foo4, NULL);
-    op3.add_dependency(&op);
-    op3.add_dependency(&op);
-    pool.async(&op3);
-
-    //run_Operation_async(queue, &op2);
-    //dispatch_group_t group = dispatch_group_create()
-    //foo(^(void){ printf("!!!!!!\n");});
-    sleep(1);
-    //tmpfoo();
-    std::cout<<"THE END";
-    return 0;
 }
