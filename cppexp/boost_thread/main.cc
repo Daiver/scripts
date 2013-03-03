8b0b750c55a35c2d3ce8c04cae534f0999f106a2
Daiver <ra22341@ya.ru>
2013-03-03 21:17:41 +0400

    minor changes

--- cppexp/boost_thread/main.cc
+++ cppexp/boost_thread/main.cc
@@ -1,5 +1,6 @@
 #include <boost/thread/thread.hpp>
 #include <iostream>
+#include <boost/bind.hpp>
 
 void hello()
 {
@@ -8,9 +9,18 @@ void hello()
     << std::endl;
 }
 
+void hello2(int i)
+{
+    std::cout <<
+        "Hello " << i
+    << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     boost::thread thrd(&hello);
+    boost::thread thrd2(boost::bind(&hello2, 12));
     thrd.join();
+    thrd2.join();
     return 0;
 }
