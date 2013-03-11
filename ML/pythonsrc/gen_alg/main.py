dafff867c9ed5499d0150b771ca6778a0822cbba
Daiver <ra22341@ya.ru>
2013-03-11 10:05:10 +0400

    minor changes

--- ML/pythonsrc/gen_alg/main.py
+++ ML/pythonsrc/gen_alg/main.py
@@ -2,10 +2,10 @@
 from random import random
 
 def f(p):
-    return p[0]*2 + p[1]*3 + p[2]*p[5] - 10*p[3]*p[4]
+    return p[0]*2 + p[1]*3 + p[2]*p[5] - 5*p[3]*p[4]
 
 def fitness(p):
-    return f(p)- 9.5
+    return f(p) - 1009
 
 def mutate(p):
     res = p[:]
@@ -22,10 +22,13 @@ def cross(p1, p2):
 
 def gen_alg(p):
     best_err = abs(fitness(p))
-    max_pop = 20
-    pop = [mutate(p) for i in xrange(max_pop)]
-    while abs(fitness(pop[0])) > 0.0001:
-        pop.sort(key=lambda x:abs(fitness(x)))
+    max_pop = 1000
+    #pop = [mutate(p) for i in xrange(max_pop)]
+    j = 0
+    pop = [p for i in xrange(max_pop)]
+    while abs(fitness(pop[0])) > 0.000001:
+        j += 1
+        pop = [mutate(p) if random() > 0.5 else p for p in pop] 
         parents = pop[:max_pop/2]
         pop = []
         for i in xrange(max_pop):
@@ -33,13 +36,13 @@ def gen_alg(p):
                 cross(parents[int(random()*len(parents))]
                 , parents[int(random()*len(parents))])
             )
-        pop = [mutate(p) if random() > 0.5 else p for p in pop] 
+        pop.sort(key=lambda x:abs(fitness(x)))
         #for p in pop:print p, fitness(p)
         #raw_input()
-    return pop[0]
+    return j, pop[0]
 
 if __name__ == '__main__':
-    p = gen_alg([1, 1, 1, 1, 1, 1])
+    i, p = gen_alg([1, 1, 1, 1, 1, 1])
     print('RES')
-    print p, f(p)
+    print i, p, f(p)
 
