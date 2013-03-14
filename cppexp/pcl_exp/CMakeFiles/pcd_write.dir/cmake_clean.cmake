bc5935b0849ec8320841d2ab8591fcd05e594cdd
Daiver <ra22341@ya.ru>
2013-03-14 10:40:04 +0400

    add my first pcl examples

---
+++ cppexp/pcl_exp/CMakeFiles/pcd_write.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcd_write.dir/pcd_write.cpp.o"
+  "pcd_write.pdb"
+  "pcd_write"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcd_write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
