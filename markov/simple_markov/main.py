0397bec2a366e8163ec43cbe2f3fd57a064d0bbc
Daiver <ra22341@ya.ru>
2013-03-15 22:01:25 +0400

    tmp work

---
+++ markov/simple_markov/main.py
@@ -0,0 +1,16 @@
+import sys
+
+def make_markov(text):
+    res = {}
+    text = text.split(' ')
+    for i in xrange(1, len(text) - 1):
+        if (text[i - 1], text[i]) not in res: res[(text[i - 1], text[i])] = []
+        res[(text[i - 1], text[i])].append(text[i + 1])
+    return res
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        text = ' '.join(open(sys.argv[1]).read().split('\n'))
+        print(text)
+    else:
+        print('NO')
