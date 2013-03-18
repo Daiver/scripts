b09622352b20a2973a890180a777aa2dd391e9ea
Daiver <ra22341@ya.ru>
2013-03-18 23:34:24 +0400

    well...

--- markov/viterbi/Matrix.h
+++ markov/viterbi/Matrix.h
@@ -4,7 +4,12 @@
 class Matrix
 {
 public: 
-    
+    Matrix(long width, long height, double *value);
+    Matrix(long width, long height); //ZEROS
+
+    bool set_element(long row, long col, double value);
+    double get_element(long row, long col); //!
+    void print();
 protected:
     long width;
     long height;
--- markov/viterbi/main.cc
+++ markov/viterbi/main.cc
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <vector>
+#include <string>
 #include "Matrix.h"
+
+const char *states[2] = {"health", "fever"};
+int num_of_states = 2;
+
+std::vector<int> obs_seq;
+double trans_m[4] = {0.7, 0.3, 0.4, 0.6};
+Matrix trans(2, 2, trans_m);
+
+double start_m[2] = {0.6, 0.4};
+Matrix start_prob(1, 2, start_m);
+
+std::vector<int> Viterbi(std::vector<int> obs_seq)
+{
+    std::vector<std::vector<int> > path;
+    std::vector<std::vector<double> > V;
+    
+
+    return path[0];
+}
 
 int main(int argc, char** argv)
 {
     printf("HI!\n");
+    trans.print();
+    start_prob.print();
     return 0;
 }
