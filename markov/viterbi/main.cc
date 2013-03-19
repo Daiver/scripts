6f0fc0fe3fd179b2b4a733dc525aeed138abd567
Daiver <ra22341@ya.ru>
2013-03-19 22:50:16 +0400

    l

--- markov/viterbi/main.cc
+++ markov/viterbi/main.cc
@@ -33,17 +33,24 @@ void Viterbi(std::vector<int> obs_seq)
 
     for(int i_obs = 1; i_obs < obs_seq.size(); i_obs++)
     {
-       for(int j_state = 0; j_state < num_of_states; j_state++)
-       {
+        std::vector<std::vector<int> > new_path;
+        for(int j_state = 0; j_state < num_of_states; j_state++)
+        {
+            std::vector<int> tmp_path;
             double max_value = 0.0;
+            int st = -1;
             for(int old_state = 0; old_state < num_of_states; old_state++)
             {
                 double tmp = emission.get_element(j_state, obs_seq[i_obs]) * V[i_obs - 1][old_state] * trans.get_element(old_state, j_state);
-                if (tmp > max_value) max_value = tmp;
+                if (tmp > max_value) {max_value = tmp;st = old_state;}
             }
             tmp_V[j_state] = max_value;
-       }
-       V.push_back(tmp_V);
+            tmp_path.insert(tmp_path.begin(), path[j_state].begin(), path[j_state].end());
+            tmp_path.push_back(st);
+            new_path.push_back(tmp_path);
+        }
+        path = new_path;
+        V.push_back(tmp_V);
     }
     
     for(int j = 0; j < V.size(); j++)
@@ -55,6 +62,14 @@ void Viterbi(std::vector<int> obs_seq)
         printf("\n");
     }
     //return path[0];
+    for(int j = 0; j < path.size(); j++)
+    {
+        for(int i = 0; i < path[j].size(); i++)
+        {
+            printf("%d ", path[j][i]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char** argv)
