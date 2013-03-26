6d78b821ed25ee98a74fe36913d8452e0c1691a8
Daiver <ra22341@ya.ru>
2013-03-26 18:37:31 +0400

    n

--- cpp2013/hashtable/hashtable.h
+++ cpp2013/hashtable/hashtable.h
@@ -1,5 +1,20 @@
 #include <pthread.h>
 
+template <class K, class V>
+class HashItem
+{
+public:
+    HashItem(const K &key, const V &value)
+    {
+        this->value = value;
+        this->key = key;
+    }
+private:
+    K key;
+    V value;
+    HashItem<K, V> *next;
+};
+
 template <class K, class V, long H(K)>
 class HashTable
 {
@@ -8,15 +23,19 @@ public:
     {
         this->sizeOfTable = 300;
         this->size = 0;
-        this->values = new V[sizeOfTable];
+        this->values = new HashItem<K, V> *[sizeOfTable];
+        for(int i = 0; i < this->sizeOfTable; i++)
+            this->values[i] = NULL;
     }
     ~HashTable()
     {
+        for(int i = 0; i < this->sizeOfTable; i++)
+            delete this->values[i];
         delete[] this->values;
     }
 
 private:
     long sizeOfTable;
     long size;
-    V* values;
+    HashItem<K, V>** values;
 };
