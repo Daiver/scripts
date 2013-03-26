bc865d641ddc4dfd67f03f888830ed85cd58f055
Daiver <ra22341@ya.ru>
2013-03-26 18:25:44 +0400

    lalala

--- cpp2013/hashtable/hashtable.h
+++ cpp2013/hashtable/hashtable.h
@@ -1,7 +1,22 @@
 #include <pthread.h>
 
-template <class K, class V>
+template <class K, class V, long H(K)>
 class HashTable
 {
-    
+public:
+    HashTable()
+    {
+        this->sizeOfTable = 300;
+        this->size = 0;
+        this->values = new V[sizeOfTable];
+    }
+    ~HashTable()
+    {
+        delete[] this->values;
+    }
+
+private:
+    long sizeOfTable;
+    long size;
+    V* values;
 };
--- cpp2013/hashtable/main.cc
+++ cpp2013/hashtable/main.cc
@@ -2,9 +2,14 @@
 
 #include "hashtable.h"
 
+long someFunc(int i)
+{
+    return i;
+}
+
 int main(int argc, char** argv)
 {
-    HashTable<int, int> ht;
+    HashTable<int, int, someFunc> ht;
     printf("and so on\n");
     return 0;
 }
