86accb22816ad5b0ecb3a4d3d52170f94ee20430
Daiver <ra22341@ya.ru>
2013-03-26 19:17:50 +0400

    ans so on

--- cpp2013/hashtable/main.cc
+++ cpp2013/hashtable/main.cc
@@ -36,6 +36,33 @@ bool testLinkedListFind(LinkedList<int, int> *list)
     return true; 
 }
 
+bool testLinkedListErase(LinkedList<int, int> *list)
+{
+    list->erase(1);
+    if(list->find(5)->value != 5) return false;
+    if(list->find(2)->value != 2) return false;
+    if(list->find(3)->value != 3) return false;
+    if(list->find(1) != NULL) return false;
+    if(list->find(3)->value != 3) return false;
+    if(list->find(2)->value != 2) return false;
+    if(list->find(6) != NULL) return false;
+    if(list->find(1) != NULL) return false;
+    list->erase(5);
+    if(list->find(5) != NULL) return false;
+    if(list->find(2)->value != 2) return false;
+    if(list->find(3)->value != 3) return false;
+    if(list->find(1) != NULL) return false;
+    if(list->find(3)->value != 3) return false;
+    if(list->find(2)->value != 2) return false;
+    if(list->find(6) != NULL) return false;
+    if(list->find(1) != NULL) return false;
+    list->erase(3);
+    if(list->find(3) != NULL) return false;
+    if(list->find(2)->value != 2) return false;
+    return true; 
+}
+
+
 void printTestRes(bool res, std::string test_name)
 {
     printf("Test %s ", test_name.c_str());
@@ -47,7 +74,9 @@ void runAllTests()
 {
     LinkedList<int, int> *list = new LinkedList<int, int>();
     printTestRes(testLinkedListAdd(list), "LinkedList add test");
-    printTestRes(testLinkedListFind(list), "LinkedList add test");
+    printTestRes(testLinkedListFind(list), "LinkedList find test");
+    printTestRes(testLinkedListErase(list), "LinkedList erase test");
+    delete list;
 }
 
 int main(int argc, char** argv)
