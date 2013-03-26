38715d426acb17c9fdbf2e67d253ccbf6753a64c
Daiver <ra22341@ya.ru>
2013-03-26 22:57:00 +0400

    long way down

--- cpp2013/hashtable/test.cpp
+++ cpp2013/hashtable/test.cpp
@@ -94,18 +94,47 @@ void printTestRes(bool res, std::string test_name)
     else printf("FAILED\n");
 }
 
-void runAllTests()
+void testLinkedList()
 {
     LinkedList<int, int> *list = new LinkedList<int, int>();
     printTestRes(testLinkedListAdd(list), "LinkedList add test");
     printTestRes(testLinkedListFind(list), "LinkedList find test");
     printTestRes(testLinkedListErase(list), "LinkedList erase test");
     printTestRes(testLinkedListErase2(list), "LinkedList erase2 test");
-    printTestRes(testLinkedListAdd(list), "LinkedList add test");
-    printTestRes(testLinkedListFind(list), "LinkedList find test");
-    printTestRes(testLinkedListErase(list), "LinkedList erase test");
-    printTestRes(testLinkedListErase2(list), "LinkedList erase2 test");
+    printTestRes(testLinkedListAdd(list), "2 LinkedList add test");
+    printTestRes(testLinkedListFind(list), "2 LinkedList find test");
+    printTestRes(testLinkedListErase(list), "2 LinkedList erase test");
+    printTestRes(testLinkedListErase2(list), "2 LinkedList erase2 test");
+    printTestRes(testLinkedListAdd(list), "3 LinkedList add test");
+    printTestRes(testLinkedListFind(list), "3 LinkedList find test");
+    printTestRes(testLinkedListErase(list), "3 LinkedList erase test");
+    printTestRes(testLinkedListErase2(list), "3 LinkedList erase2 test");
     delete list;
 }
 
 
+long strHash(std::string string)
+{
+    size_t len = string.size();
+    long hash = 0;
+    for(size_t i = 0; i < len; ++i) 
+        hash = 65599 * hash + string[i];
+    return hash ^ (hash >> 16);
+}
+
+bool testHashTableAdd(HashTable<std::string, int, strHash> *table)
+{
+    std::string res = "A";
+    table->add(res, 12);
+    return true;
+}
+
+void testHashTable()
+{
+    
+}
+
+void runAllTests()
+{
+    testLinkedList();
+}
