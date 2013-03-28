35a93697be0d342c2296d3d594d22af642fd1932
Daiver <ra22341@ya.ru>
2013-03-28 13:11:19 +0400

    oh my god

--- cppexp/py_gcd/libpygcd.cpp
+++ cppexp/py_gcd/libpygcd.cpp
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <dispatch/dispatch.h>
 
+void work3(int(*f)(void))
+{
+    printf(">%d\n", f());
+}
+
+void work2(int i, int j)
+{
+    printf("Hello world %d %d\n", i, j);
+}
+
 void work()
 {
     dispatch_queue_t queue = dispatch_queue_create("com.mydomain.myapp.longrunningfunction", DISPATCH_QUEUE_CONCURRENT);
     dispatch_async(queue, ^(void) {
         printf("Hello world2\n");
     });
+    dispatch_async(queue, ^(void) {
+        printf("Hello world3\n");
+    });
+    dispatch_async(queue, ^(void) {
+        printf("Hello world4\n");
+    });
+    dispatch_async(queue, ^(void) {
+        printf("Hello world5\n");
+    });
+    
     printf("Hello world\n");
 }
--- cppexp/py_gcd/run.py
+++ cppexp/py_gcd/run.py
@@ -1,3 +1,6 @@
 import ctypes
 lib = ctypes.cdll.LoadLibrary('./libpygcd.so.1.0')
-lib._Z4workv()
+#lib._Z4workv()
+#lib._Z5work2ii(2, 4)
+def f(): return 10000
+lib._Z5work3PFivE(ctypes.CFUNCTYPE(ctypes.c_int)(f))
