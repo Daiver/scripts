3be3498e246e1762faef22f4e06e1f9d185c3c7f
Daiver <ra22341@ya.ru>
2013-03-31 14:47:53 +0400

    yeah

--- cpp2013/cpptalk/main.cc
+++ cpp2013/cpptalk/main.cc
@@ -2,6 +2,20 @@
 #include <vector>
 #include <stdio.h>
 #include <string>
+#include <algorithm>
+#include <functional>
+
+void autoTest();
+void forTest();
+void lambdaTest();
+
+int main(int argc, char** argv)
+{
+    //autoTest();
+    lambdaTest();
+    //forTest();
+    return 0;
+}
 
 template<class A, class B>
 auto sum(A a, B b) -> decltype(a + b)
@@ -38,6 +52,53 @@ void autoTest()
     printf("Sum test4 %f\n", sum(1, 2.5));
 }
 
+
+void lambdaTest()
+{
+    std::vector<std::string> vec;
+    vec.push_back("Be");
+    vec.push_back("or");
+    vec.push_back("not");
+    vec.push_back("to");
+    vec.push_back("be");
+    
+    std::for_each(vec.begin(), vec.end(), [](std::string s){
+        std::cout<<s<<" ";
+    });
+    std::cout<<std::endl;
+    
+    char separator = '_';
+    std::for_each(vec.begin(), vec.end(), [separator](std::string s){
+        std::cout<<s<<separator;
+    });
+    std::cout<<std::endl;
+
+    separator = '|';
+    std::for_each(vec.begin(), vec.end(), [&separator](std::string s){
+        std::cout<<s<<separator;
+        separator = ' ';
+    });
+    std::cout<<std::endl;
+    
+    auto f1 = [](double a, double b) -> double
+    {
+        return a + b;
+    };
+    std::cout<<"f1 "<<f1(3, 2)<<std::endl;
+    
+    auto f2 = [f1](double a) -> double
+    {
+        return f1(a, 100);
+    };
+    
+    std::cout<<"f2 "<<f2(3)<<std::endl;
+    auto f3 = [](std::function<double(double)> f1, double a) -> double
+    {
+        return f1(a)*10;
+    };
+    std::cout<<"f3 "<<f3(f2, 3)<<std::endl;
+}
+
 void forTest()
 {
     std::vector<std::string> vec;
@@ -49,18 +110,11 @@ void forTest()
         std::cout<<s<<" ";
     }
     std::cout<<std::endl;
-
+    /*
     for(auto &s : vec)
     {
         std::cout<<s<<" ";
     }
-    std::cout<<std::endl;
+    std::cout<<std::endl;*/
 }
 
-
-int main(int argc, char** argv)
-{
-    autoTest();
-    forTest();
-    return 0;
-}
