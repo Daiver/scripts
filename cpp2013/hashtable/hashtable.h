423295c97f1d6452efdaa0451c01a0d7c5dd1ce8
Daiver <ra22341@ya.ru>
2013-03-31 16:25:13 +0400

    1234578

--- cpp2013/hashtable/hashtable.h
+++ cpp2013/hashtable/hashtable.h
@@ -58,79 +58,95 @@ public:
         }
     }
 
-    V* get(const K& key)
-    {
-        long index = this->getIndex(key);
-        ListItem<K, V> *tmp = this->values[index]->find(key);
-        if(NULL == tmp) return NULL;
-        return &tmp->value;
-    }
-
-    V getOrDef(const K& key, const V& def)
-    {
-        V* tmp = this->get(key);
-        if(NULL == tmp) return def;
-        return *tmp;
-    }
-
-    bool erase(const K& key)
-    {
-        long index = this->getIndex(key);
-        bool res = this->values[index]->erase(key);
-        this->linked_values->erase(key);
-        if(res)
-        {
-            this->_size--;
-            return true;
-        }
-        return false;
-    }
-
-    ~HashTable()
-    {
-        this->clear();
-    }
-
-    long size()
-    {
-        return this->_size;
-    }
-
-    void reset()
-    {
-        this->clear();
-        this->_size = 0;
-        this->create();
-    }
-
+    V* get(const K& key);
+    V getOrDef(const K& key, const V& def);
+    bool erase(const K& key);
+    ~HashTable();
+    long size();
+    void reset();
 private:
-    long getIndex(const K& key)
-    {
-        return H(key) % this->sizeOfTable;
-    }
-
-    void clear()
-    {
-        delete this->linked_values;
-        for(int i = 0; i < this->sizeOfTable; i++)
-            delete this->values[i];
-        delete[] this->values;
-    }
-
-    void create()
-    {
-        this->linked_values = new LinkedList<K, ListItem<K, V> *>();
-        this->values = new LinkedList<K, V> *[sizeOfTable];
-        for(int i = 0; i < this->sizeOfTable; i++)
-            this->values[i] = new LinkedList<K, V>();
-    }
-
-
+    long getIndex(const K& key);
+    void clear();
+    void create();
     long sizeOfTable;
     long _size;
     LinkedList<K, V>** values;
     LinkedList<K, ListItem<K, V> *> *linked_values;
 };
 
+template <class K, class V, long H(K)>
+V* HashTable<K, V, H>::get(const K& key)
+{
+    long index = this->getIndex(key);
+    ListItem<K, V> *tmp = this->values[index]->find(key);
+    if(NULL == tmp) return NULL;
+    return &tmp->value;
+}
 
+template <class K, class V, long H(K)>
+V HashTable<K, V, H>::getOrDef(const K& key, const V& def)
+{
+    V* tmp = this->get(key);
+    if(NULL == tmp) return def;
+    return *tmp;
+}
+
+template <class K, class V, long H(K)>
+bool HashTable<K, V, H>::erase(const K& key)
+{
+    long index = this->getIndex(key);
+    bool res = this->values[index]->erase(key);
+    this->linked_values->erase(key);
+    if(res)
+    {
+        this->_size--;
+        return true;
+    }
+    return false;
+}
+
+template <class K, class V, long H(K)>
+HashTable<K, V, H>::~HashTable()
+{
+    this->clear();
+}
+
+template <class K, class V, long H(K)>
+long HashTable<K, V, H>::size()
+{
+    return this->_size;
+}
+
+template <class K, class V, long H(K)>
+void HashTable<K, V, H>::reset()
+{
+    this->clear();
+    this->_size = 0;
+    this->create();
+}
+
+template <class K, class V, long H(K)>
+long HashTable<K, V, H>::getIndex(const K& key)
+{
+    return H(key) % this->sizeOfTable;
+}
+
+
+template <class K, class V, long H(K)>
+void HashTable<K, V, H>::create()
+{
+    this->linked_values = new LinkedList<K, ListItem<K, V> *>();
+    this->values = new LinkedList<K, V> *[sizeOfTable];
+    for(int i = 0; i < this->sizeOfTable; i++)
+        this->values[i] = new LinkedList<K, V>();
+}
+
+template <class K, class V, long H(K)>
+void HashTable<K, V, H>::clear()
+{
+    delete this->linked_values;
+    for(int i = 0; i < this->sizeOfTable; i++)
+        delete this->values[i];
+    delete[] this->values;
+}
 #endif
