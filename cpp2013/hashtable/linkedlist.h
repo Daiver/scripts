1e4e8dc37f37f05e7783b869f60ad091184b15a1
Daiver <ra22341@ya.ru>
2013-03-31 15:58:38 +0400

    and so on

--- cpp2013/hashtable/linkedlist.h
+++ cpp2013/hashtable/linkedlist.h
@@ -44,45 +44,11 @@ public:
         return tmp;
     }
 
-    bool erase(const K& key)
-    {
-        if(this->tail == NULL) return false;
-        if(this->tail->key == key)
-        {
-            ListItem<K, V> *tmp = this->tail;
-            this->tail = this->tail->next;
-            delete tmp;
-            this->_size--;
-            return true;
-        }
-        ListItem<K, V> *old = this->tail;
-        ListItem<K, V> *tmp = this->tail->next;
-        while((tmp != NULL) && (tmp->key != key))
-        {
-            old = tmp;
-            tmp = tmp->next;
-        }
-        if (NULL == tmp) return false;
-        old->next = tmp->next;
-        delete tmp;
-        this->_size--;
-        return true;
-    }
+    bool erase(const K& key);
 
-    ListItem<K, V> *add(const K& key, const V& value)
-    {
-        ListItem<K, V> *tmp = new ListItem<K, V>(key, value);
-        return this->add(tmp);
-    }
 
-    ListItem<K, V> *add(ListItem<K, V> *tmp)
-    {
-        tmp->next = this->tail;
-        this->tail = tmp;
-        this->_size++;
-        return tmp;
-    }
-
+    ListItem<K, V> *add(const K& key, const V& value);
+    ListItem<K, V> *add(ListItem<K, V> *tmp);
     long size();
 
 private:
@@ -90,10 +56,53 @@ private:
     long _size;
 };
 
+
+template<class K, class V>
+bool LinkedList<K, V>::erase(const K& key)
+{
+    if(this->tail == NULL) return false;
+    if(this->tail->key == key)
+    {
+        ListItem<K, V> *tmp = this->tail;
+        this->tail = this->tail->next;
+        delete tmp;
+        this->_size--;
+        return true;
+    }
+    ListItem<K, V> *old = this->tail;
+    ListItem<K, V> *tmp = this->tail->next;
+    while((tmp != NULL) && (tmp->key != key))
+    {
+        old = tmp;
+        tmp = tmp->next;
+    }
+    if (NULL == tmp) return false;
+    old->next = tmp->next;
+    delete tmp;
+    this->_size--;
+    return true;
+}
+
 template<class K, class V>
 long LinkedList<K, V>::size()
 {
     return this->_size;
+}
+
+template<class K, class V>
+ListItem<K, V> *LinkedList<K, V>::add(ListItem<K, V> *tmp)
+{
+    tmp->next = this->tail;
+    this->tail = tmp;
+    this->_size++;
+    return tmp;
+}
+
+template<class K, class V>
+ListItem<K, V> *LinkedList<K, V>::add(const K& key, const V& value)
+{
+    ListItem<K, V> *tmp = new ListItem<K, V>(key, value);
+    return this->add(tmp);
 }
 
 #endif
