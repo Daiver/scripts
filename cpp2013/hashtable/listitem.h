b354fb250fbfc9cc2e33e9208cd0693de2c63d7e
Daiver <ra22341@ya.ru>
2013-03-31 10:57:52 +0400

    separate listitem implementation

--- cpp2013/hashtable/listitem.h
+++ cpp2013/hashtable/listitem.h
@@ -5,63 +5,77 @@ template <class K, class V>
 class ListItem
 {
 public:
-    ListItem(const K &key, const V &value)
-    {
-        this->value = value;
-        this->key = key;
-        this->next = NULL;
-    }
-    ~ListItem()
-    {
-        //delete this->next;
-    }
+    ListItem(const K &key, const V &value);
 
     ListItem<K, V> *next;
     K key;
     V value;
 };
 
+template<class K, class V>
+ListItem<K, V>::ListItem(const K &key, const V &value)
+{
+    this->value = value;
+    this->key = key;
+    this->next = NULL;
+}
+
 template <class K, class V>
 class ListItemIterator
 {
 public:
-    ListItemIterator(ListItem<K, V>* item)
-    {
-        inner_item = item;
-    }
+    ListItemIterator(ListItem<K, V>* item);
 
-    ListItemIterator& operator++ (int)
-    {
-        ListItemIterator<K, V> tmp(*this);
-        if(NULL != this->inner_item)
-            this->inner_item = this->inner_item->next;
-        return tmp;
-
-    }
-    
-    V& operator *()
-    {
-        return this->inner_item->value;
-    }
-
-    ListItemIterator& operator++ ()
-    {
-        if(NULL != this->inner_item)
-            this->inner_item = this->inner_item->next;
-        return *this;
-    }
-
-    bool operator==(const ListItemIterator<K, V>& o) const
-    {
-        return this->inner_item == o.inner_item;
-    }
-    bool operator!=(const ListItemIterator<K, V>& o) const { return !(*this == o); }
-
+    ListItemIterator& operator++ (int);
+    V& operator *();
+    ListItemIterator& operator++ ();
+    bool operator==(const ListItemIterator<K, V>& o) const;
+    bool operator!=(const ListItemIterator<K, V>& o) const;
 
 protected:
     ListItem<K, V> *inner_item;
 };
 
+template<class K, class V>
+ListItemIterator<K, V>::ListItemIterator(ListItem<K, V>* item)
+{
+    inner_item = item;
+}
+
+template<class K, class V>
+ListItemIterator<K, V>& ListItemIterator<K, V>::operator++ (int)
+{
+    ListItemIterator<K, V> tmp(*this);
+    if(NULL != this->inner_item)
+        this->inner_item = this->inner_item->next;
+    return tmp;
+}
+
+template<class K, class V>
+V& ListItemIterator<K, V>::operator *()
+{
+    return this->inner_item->value;
+}
+
+template<class K, class V>
+ListItemIterator<K, V>& ListItemIterator<K, V>::operator++ ()
+{
+    if(NULL != this->inner_item)
+        this->inner_item = this->inner_item->next;
+    return *this;
+}
+
+template<class K, class V>
+bool ListItemIterator<K, V>::operator==(const ListItemIterator<K, V>& o) const 
+{
+    return this->inner_item == o.inner_item;
+}
+
+template<class K, class V>
+bool ListItemIterator<K, V>::operator!=(const ListItemIterator<K, V>& o) const 
+{ 
+    return !(*this == o); 
+}
 
 #endif
 
