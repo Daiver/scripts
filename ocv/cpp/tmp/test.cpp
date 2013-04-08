3dcc27dfa4e06a131c5cb27fb96dc2a31da8f15d
Daiver <ra22341@ya.ru>
2013-04-08 23:00:43 +0400

    hate cxx dependences

--- ocv/cpp/tmp/test.cpp
+++ ocv/cpp/tmp/test.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 int main() {
     char var;
     cv::Mat img;
+    cv::StereoBM(0);
     img = cv::imread("../tmp2/Image0.jpg");
     cv::namedWindow("Image");
     cv::imshow("Image", img);
