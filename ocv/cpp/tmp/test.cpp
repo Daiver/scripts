aa3ae3892daf30fc441473a9ec4aa1f974fcf6ca
Daiver <ra22341@ya.ru>
2013-04-08 23:18:01 +0400

    and so on

--- ocv/cpp/tmp/test.cpp
+++ ocv/cpp/tmp/test.cpp
@@ -7,15 +7,18 @@ int main() {
     cv::StereoBM bm(CV_STEREO_BM_NORMALIZED_RESPONSE);
     //cv::Mat img;
     //img = cv::imread("../tmp2/Image0.jpg");
-    cv::Mat left  = cv::imread("tsukuba/scene1.row3.col3.ppm");
-    cv::Mat right = cv::imread("tsukuba/scene1.row3.col5.ppm");
+    cv::Mat left  = cv::imread("tsukuba/scene1.row3.col3.ppm", CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat right = cv::imread("tsukuba/scene1.row3.col5.ppm", CV_LOAD_IMAGE_GRAYSCALE);
     cv::Mat res; 
     bm(left, right, res);
+    std::cout<<res;
 
     cv::namedWindow("left");
     cv::imshow("left", left);
     cv::namedWindow("right");
     cv::imshow("right", right);
+    cv::namedWindow("depth");
+    cv::imshow("depth", res);
     cv::waitKey();
     return 0;
 }
