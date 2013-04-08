7f1ba4acf8e7905e174ec3ea15e44a2ae2eba839
Daiver <ra22341@ya.ru>
2013-04-08 23:07:18 +0400

    and so on

--- ocv/cpp/tmp/test.cpp
+++ ocv/cpp/tmp/test.cpp
@@ -4,11 +4,18 @@
 #include <opencv2/calib3d/calib3d.hpp>
 int main() {
     char var;
-    cv::Mat img;
-    cv::StereoBM(0);
-    img = cv::imread("../tmp2/Image0.jpg");
-    cv::namedWindow("Image");
-    cv::imshow("Image", img);
+    cv::StereoBM bm(CV_STEREO_BM_NORMALIZED_RESPONSE);
+    //cv::Mat img;
+    //img = cv::imread("../tmp2/Image0.jpg");
+    cv::Mat left  = cv::imread("tsukuba/scene1.row3.col3.ppm");
+    cv::Mat right = cv::imread("tsukuba/scene1.row3.col5.ppm");
+    cv::Mat res; 
+    bm(left, right, res);
+
+    cv::namedWindow("left");
+    cv::imshow("left", left);
+    cv::namedWindow("right");
+    cv::imshow("right", right);
     cv::waitKey();
     return 0;
 }
