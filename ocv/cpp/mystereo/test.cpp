784abc5e2a506369e1ea246d99f968076e13bf34
Daiver <ra22341@ya.ru>
2013-04-09 13:26:14 +0400

    more changes

--- ocv/cpp/mystereo/test.cpp
+++ ocv/cpp/mystereo/test.cpp
@@ -2,15 +2,18 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-int main() {
+int main(int argc, char **argv) {
     char var;
     cv::StereoBM bm(CV_STEREO_BM_NORMALIZED_RESPONSE);
     //cv::Mat img;
     //img = cv::imread("../tmp2/Image0.jpg");
-    cv::Mat left  = cv::imread("tsukuba/scene1.row3.col3.ppm", CV_LOAD_IMAGE_GRAYSCALE);
-    cv::Mat right = cv::imread("tsukuba/scene1.row3.col5.ppm", CV_LOAD_IMAGE_GRAYSCALE);
+    auto left_name  = "tsukuba/scene1.row3.col3.ppm";
+    auto right_name = "tsukuba/scene1.row3.col5.ppm";
+    cv::Mat left  = cv::imread(left_name, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat right = cv::imread(right_name, CV_LOAD_IMAGE_GRAYSCALE);
     cv::Mat res; 
     bm(left, right, res);
+    //res[0][0] = 100;
     double min;
     double max;
     cv::minMaxIdx(res, &min, &max);
