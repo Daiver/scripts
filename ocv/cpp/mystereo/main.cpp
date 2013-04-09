82c9c0487d3cf058b37043be55c5615ad3807f73
Daiver <ra22341@ya.ru>
2013-04-09 13:50:03 +0400

    minor changes

--- ocv/cpp/mystereo/main.cpp
+++ ocv/cpp/mystereo/main.cpp
@@ -2,9 +2,26 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-int main(int argc, char **argv) {
-    char var;
+
+cv::Mat getDepthMap(cv::Mat const &left, cv::Mat const &right)
+{
+    cv::Mat res; 
     cv::StereoBM bm(CV_STEREO_BM_NORMALIZED_RESPONSE);
+    bm(left, right, res);
+    return res;
+}
+
+cv::Mat normalize(cv::Mat const &depth_map)
+{
+    double min;
+    double max;
+    cv::minMaxIdx(depth_map, &min, &max);
+    cv::Mat adjMap;
+    cv::convertScaleAbs(depth_map, adjMap, 255 / max);
+    return adjMap;
+}
+
+int main(int argc, char **argv) {
     //cv::Mat img;
     //img = cv::imread("../tmp2/Image0.jpg");
     auto left_name  = "tsukuba/scene1.row3.col3.ppm";
@@ -16,14 +33,9 @@ int main(int argc, char **argv) {
     }
     cv::Mat left  = cv::imread(left_name, CV_LOAD_IMAGE_GRAYSCALE);
     cv::Mat right = cv::imread(right_name, CV_LOAD_IMAGE_GRAYSCALE);
-    cv::Mat res; 
-    bm(left, right, res);
     //res[0][0] = 100;
-    double min;
-    double max;
-    cv::minMaxIdx(res, &min, &max);
-    cv::Mat adjMap;
-    cv::convertScaleAbs(res, adjMap, 255 / max);
+    cv::Mat map = getDepthMap(left, right);
+    cv::Mat adjMap = normalize(map);
     cv::imshow("Out", adjMap);
 
     cv::imshow("left", left);
