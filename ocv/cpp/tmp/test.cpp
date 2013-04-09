d07913a091f3eccd936017089ef5caf0185459aa
Daiver <ra22341@ya.ru>
2013-04-09 10:53:14 +0400

    and so on

--- ocv/cpp/tmp/test.cpp
+++ ocv/cpp/tmp/test.cpp
@@ -11,14 +11,24 @@ int main() {
     cv::Mat right = cv::imread("tsukuba/scene1.row3.col5.ppm", CV_LOAD_IMAGE_GRAYSCALE);
     cv::Mat res; 
     bm(left, right, res);
-    std::cout<<res;
+    double min;
+    double max;
+    cv::minMaxIdx(res, &min, &max);
+    cv::Mat adjMap;
+    cv::convertScaleAbs(res, adjMap, 255 / max);
+    cv::imshow("Out", adjMap);
+    /*cv::Scalar norm = cv::sum(res);
+    int norm_2 = norm[0];
+    std::cout<<norm_2;
+    res /= norm_2;
+    std::cout<<res;*/
 
     cv::namedWindow("left");
     cv::imshow("left", left);
     cv::namedWindow("right");
     cv::imshow("right", right);
-    cv::namedWindow("depth");
-    cv::imshow("depth", res);
+    //cv::namedWindow("depth");
+    //cv::imshow("depth", res);
     cv::waitKey();
     return 0;
 }
