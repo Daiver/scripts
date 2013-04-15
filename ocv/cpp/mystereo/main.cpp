eb203b8c7f67b12070cf5e6118c43ed22efbf991
Daiver <ra22341@ya.ru>
2013-04-15 14:26:36 +0400

    split to 2 functions

--- ocv/cpp/mystereo/main.cpp
+++ ocv/cpp/mystereo/main.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-#include "opencv2/contrib/contrib.hpp"
+#include <opencv2/contrib/contrib.hpp>
 #include <vector>
 #include <queue>
 
