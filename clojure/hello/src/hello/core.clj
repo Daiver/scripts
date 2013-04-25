e279911cfbe1314d209c80f7d5896d3ca2b75935
Daiver <ra22341@ya.ru>
2013-04-25 22:47:20 +0400

    and so on

--- clojure/hello/src/hello/core.clj
+++ clojure/hello/src/hello/core.clj
@@ -1,25 +1,29 @@
 (ns hello.core)
 (defn -main
-    "I don't do a whole lot."
+    ""
     [& args]
     (clojure-version)
-    (println (count args))
-    (println (args 1))
-    (println 
-        (filter #(> % 10)
-            (map
-                #(* % 10)
-                (map read-string args)
+    (println (#(* 10 %1) 10))
+    (comment
+        (println "args count" (count args))
+        ;(println  (next (next args)))
+        (println 
+            (filter #(> % 10)
+                (map
+                    #(* % 10)
+                    (map read-string args)
+                )
             )
         )
+        (println
+            (map
+                #(int %1)
+                (map 
+                    #(+ %1 %2 ) '(1 2 3 4) '(2 2 2 2)
+                )
+            )
+        )
+        (.substring s (.indexOf s "(") (+ (.indexOf s ")") 1) )
     )
-    ;(println
-    ;    (map
-    ;        #(int %1)
-    ;        (map 
-    ;            #(+ %1 %2 ) '(1 2 3 4) '(2 2 2 2)
-    ;        )
-    ;    )
-    ;)
 )
 
