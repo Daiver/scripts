d5d1a3e6dc237282a7a5669365b4f017b3428fdd
Daiver <ra22341@ya.ru>
2013-04-26 20:46:41 +0400

    and so on

--- clojure/hello/src/hello/core.clj
+++ clojure/hello/src/hello/core.clj
@@ -1,9 +1,25 @@
 (ns hello.core)
+(defn extract
+    [str]
+    (let  [f_i (.indexOf str "(")]
+        (let 
+            [
+                s_i (.indexOf (.substring str (+ f_i 1) ) "(")
+                l_i (.indexOf str ")")
+            ]
+            s_i
+            l_i
+        )
+    )
+)
+
 (defn -main
     ""
     [& args]
-    (clojure-version)
-    (println (#(* 10 %1) 10))
+    (println "version" (clojure-version))
+    (def arg (first args))
+    (println (extract arg))
+    ;(println (#(* 10 %1) 10))
     (comment
         (println "args count" (count args))
         ;(println  (next (next args)))
