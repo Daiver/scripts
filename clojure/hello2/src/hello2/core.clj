73e852891c5ab01377fcbb449d010b41650638af
Daiver <ra22341@ya.ru>
2013-05-02 17:55:02 +0400

    and so on

--- clojure/hello2/src/hello2/core.clj
+++ clojure/hello2/src/hello2/core.clj
@@ -1,5 +1,13 @@
 (ns hello2.core)
 
+(defn lazy-seq-fibo  ; (1-я строка)
+  ([] ; (2-я строка)
+     (concat [0 1] (lazy-seq-fibo 0N 1N))) ; (3-я строка)
+  ([a b] ; (4-я строка)
+     (let [n (+ a b)]                    ; (5-я строка)
+       (lazy-seq                         ; (6-я строка)
+        (cons n (lazy-seq-fibo b n)))))) ; (7-я строка)
+
 (defn foo
     [arg]
     (cond
@@ -12,7 +20,9 @@
 
 (defn -main
     [& args]
-    (println (foo 8))
+    (println (take 500 (lazy-seq-fibo)))
+    (println (take 900 (lazy-seq-fibo)))
+    ;(println (foo 8))
     ;(println args)
 
 )
