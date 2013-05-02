50c516e15dfa14d05cf19fa47d44aa96231c712c
Daiver <ra22341@ya.ru>
2013-05-02 15:20:35 +0400

    cond test

--- clojure/hello2/src/hello2/core.clj
+++ clojure/hello2/src/hello2/core.clj
@@ -1,7 +1,18 @@
 (ns hello2.core)
+
+(defn foo
+    [arg]
+    (cond
+        (== arg 1) "A"
+        (== arg 2) "B"
+        (== arg 3) "C"
+        :else "NOPE"
+    )
+)
+
 (defn -main
     [& args]
-
+    (println (foo 8))
     ;(println args)
 
 )
