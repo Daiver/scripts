668028822734c311aca332f1232e663596db37a0
Daiver <ra22341@ya.ru>
2013-05-04 20:08:03 +0400

    and so on

--- clojure/rcr/src/rcr/core.clj
+++ clojure/rcr/src/rcr/core.clj
@@ -1,35 +1,31 @@
 (ns rcr.core)
 
+(defn make_tokens
+    [s]
+    (clojure.string/split (clojure.string/replace (clojure.string/replace s #"\(" " ( ") #"\)" " ) ") #"\s"))
+
 (defn make_tree
     [tokens]
     (loop [i 0 node ()]
         (if (= i (count tokens))
             [node i]
             (let [frst (nth tokens i)]
-                (println node)
+                ;(println node)
                 (cond
                     (= frst \()
                     (let [[new_node cnt]  (make_tree (drop (inc i) tokens))]
                         (recur 
                             (+ i 1 cnt)
-                            (concat node (list new_node))
-                        )
-                    )
+                            (concat node (list new_node))))
                     (= frst \))
                     [node (inc i)]
                     :default
                         (recur 
                             (+ i 1)
-                            (concat node (list frst))
-                        )
-                )
-            )
-        )
-    )
-)
+                            (concat node (list frst))))))))
 
 (defn -main
     [& args]
-    (println args)
-    (println (make_tree "1 23 45 (8 ((qwe r) t) 7) (1) 32 3"))
+    (println (make_tokens "1 23(456) 45"))
+    (println (make_tree (make_tokens "1 23 45 (8 ((qwe r) t (123 (no hope) (no changes) )) 7) (1) 32 3")))
 )
