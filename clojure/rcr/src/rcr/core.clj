3ef8dd78b2071ef4e9781ed84c7e80abb1d0e704
Daiver <ra22341@ya.ru>
2013-05-07 11:10:51 +0400

    and so on

--- clojure/rcr/src/rcr/core.clj
+++ clojure/rcr/src/rcr/core.clj
@@ -4,6 +4,15 @@
     [s]
     (clojure.string/split (clojure.string/replace (clojure.string/replace s #"\(" " ( ") #"\)" " ) ") #"\s"))
 
+(defn execute
+    [arg]
+    (println (= (type arg) (type '(""))))
+    (if (= (type arg) (type '( "" ) ))
+        arg
+        (apply (resolve (symbol (first arg))) '(map execute (rest arg))) 
+    )
+)
+
 (defn make_tree
     [tokens]
     (loop [i 0 node ()]
@@ -26,8 +35,9 @@
 
 (defn -main
     [& args]
-    (println (make_tokens "1 23(456) 45"))
+    ;(println (make_tokens "1 23(456) 45"))
     ;(println (make_tree (make_tokens "1 23 45 (8 ((qwe r) t (123 (no hope) (no changes) )) 7) (1) 32 3")))
-    (def s "+ (* 2 3) 5")
+    (def s "+ (* 20 3) 5")
     (println (make_tree (make_tokens s)))
+    (println (execute '("+" 1 2 3)))
 )
