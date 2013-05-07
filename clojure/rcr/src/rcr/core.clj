48c271a734fd70859df07ecf1419ca39afa15f07
Daiver <ra22341@ya.ru>
2013-05-07 13:31:41 +0400

    ...

--- clojure/rcr/src/rcr/core.clj
+++ clojure/rcr/src/rcr/core.clj
@@ -1,17 +1,27 @@
 (ns rcr.core)
 
+(defn parse-num [s]
+    (try (Integer/parseInt s)
+        (catch NumberFormatException nfe 
+            (try (Float/parseFloat s) 
+                (catch NumberFormatException nfe s)))))
+
+(defn process-numeric-tokens [tokens]
+    (map #(parse-num %) tokens))
+
 (defn make_tokens
     [s]
     (clojure.string/split (clojure.string/replace (clojure.string/replace s #"\(" " ( ") #"\)" " ) ") #"\s"))
 
 (defn execute
     [arg]
-    ;(println (= (type arg) (type '(""))) ())
-    (if (= (type arg) (type '( "" ) ))
+    (println arg)
+    (println (type arg) )
+    (if 
+        ;(= (type arg) (type '( "" ) ))
+        (= (type arg) clojure.lang.LazySeq)
         (apply (resolve (symbol (first arg))) (map execute (rest arg))) 
-        arg
-    )
-)
+        arg))
 
 (defn make_tree
     [tokens]
@@ -38,6 +48,9 @@
     ;(println (make_tokens "1 23(456) 45"))
     ;(println (make_tree (make_tokens "1 23 45 (8 ((qwe r) t (123 (no hope) (no changes) )) 7) (1) 32 3")))
     (def s "+ (* 20 3) 5")
-    (println (make_tree (make_tokens s)))
-    (println (execute '("+" 1  ("*" 3 5))))
+    (let [[tree count] (make_tree (process-numeric-tokens (make_tokens s)))]
+        (println tree)
+        (println (execute tree))
+    )
+    ;(println (execute '("+" 1  ("*" 3 5))))
 )
