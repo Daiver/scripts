95ae9622cfe3e32fa54df61aef1dae998c3c040a
Daiver <ra22341@ya.ru>
2013-05-08 09:48:51 +0400

    switch to namespaces

--- ocv/cpp/face_train/main.cpp
+++ ocv/cpp/face_train/main.cpp
@@ -2,55 +2,60 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
 #include <iostream>
+#include <vector>
+//using namespace cv;
 
-using namespace cv;
+float *getHOG(const cv::Mat &image)
+{
+    return 0;
+}
 
 int main()
 {
     // Data for visual representation
     int width = 512, height = 512;
-    Mat image = Mat::zeros(height, width, CV_8UC3);
+    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
 
     // Set up training data
     float labels[4] = {1.0, -1.0, -1.0, -1.0};
-    Mat labelsMat(4, 1, CV_32FC1, labels);
+    cv::Mat labelsMat(4, 1, CV_32FC1, labels);
 
     float trainingData[4][2] = { {501, 10}, {255, 10}, {501, 255}, {10, 501} };
 
-    Mat trainingDataMat(4, 2, CV_32FC1, trainingData);
+    cv::Mat trainingDataMat(4, 2, CV_32FC1, trainingData);
     //std::cout << trainingDataMat.at<float>(2, 1) << "\n"; //255
 
     // Set up SVM's parameters
-    CvSVMParams params;
-    params.svm_type    = CvSVM::C_SVC;
-    params.kernel_type = CvSVM::LINEAR;
-    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+    cv::SVMParams params;
+    params.svm_type    = cv::SVM::C_SVC;
+    params.kernel_type = cv::SVM::LINEAR;
+    params.term_crit   = cv::TermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
 
     // Train the SVM
-    CvSVM SVM;
-    SVM.train(trainingDataMat, labelsMat, Mat(), Mat(), params);
+    cv::SVM SVM;
+    SVM.train(trainingDataMat, labelsMat, cv::Mat(), cv::Mat(), params);
 
-    Vec3b green(0,255,0), blue (255,0,0);
+    cv::Vec3b green(0,255,0), blue (255,0,0);
     // Show the decision regions given by the SVM
     for (int i = 0; i < image.rows; ++i)
         for (int j = 0; j < image.cols; ++j)
         {
-            Mat sampleMat = (Mat_<float>(1,2) << i,j);
+            cv::Mat sampleMat = (cv::Mat_<float>(1,2) << i,j);
             float response = SVM.predict(sampleMat);
 
             if (response == 1)
-                image.at<Vec3b>(j, i)  = green;
+                image.at<cv::Vec3b>(j, i)  = green;
             else if (response == -1)
-                 image.at<Vec3b>(j, i)  = blue;
+                 image.at<cv::Vec3b>(j, i)  = blue;
         }
 
     // Show the training data
     int thickness = -1;
     int lineType = 8;
-    circle( image, Point(501,  10), 5, Scalar(  0,   0,   0), thickness, lineType);
-    circle( image, Point(255,  10), 5, Scalar(255, 255, 255), thickness, lineType);
-    circle( image, Point(501, 255), 5, Scalar(255, 255, 255), thickness, lineType);
-    circle( image, Point( 10, 501), 5, Scalar(255, 255, 255), thickness, lineType);
+    cv::circle( image, cv::Point(501,  10), 5, cv::Scalar(  0,   0,   0), thickness, lineType);
+    cv::circle( image, cv::Point(255,  10), 5, cv::Scalar(255, 255, 255), thickness, lineType);
+    cv::circle( image, cv::Point(501, 255), 5, cv::Scalar(255, 255, 255), thickness, lineType);
+    cv::circle( image, cv::Point( 10, 501), 5, cv::Scalar(255, 255, 255), thickness, lineType);
 
     // Show support vectors
     thickness = 2;
@@ -60,12 +65,12 @@ int main()
     for (int i = 0; i < c; ++i)
     {
         const float* v = SVM.get_support_vector(i);
-        circle( image,  Point( (int) v[0], (int) v[1]),   6,  Scalar(128, 128, 128), thickness, lineType);
+        cv::circle( image,  cv::Point( (int) v[0], (int) v[1]),   6,  cv::Scalar(128, 128, 128), thickness, lineType);
     }
 
-    imwrite("result.png", image);        // save the image
+    cv::imwrite("result.png", image);        // save the image
 
-    imshow("SVM Simple Example", image); // show it to the user
-    waitKey(0);
+    cv::imshow("SVM Simple Example", image); // show it to the user
+    cv::waitKey(0);
 
 }
