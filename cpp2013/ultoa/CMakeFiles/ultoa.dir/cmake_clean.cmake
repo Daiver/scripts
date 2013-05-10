b08a7f7c283ade22ad79b92ed48021eb73ebceae
Daiver <ra22341@ya.ru>
2013-05-10 13:01:15 +0400

    and so on

---
+++ cpp2013/ultoa/CMakeFiles/ultoa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ultoa.dir/main.cpp.o"
+  "ultoa.pdb"
+  "ultoa"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ultoa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
