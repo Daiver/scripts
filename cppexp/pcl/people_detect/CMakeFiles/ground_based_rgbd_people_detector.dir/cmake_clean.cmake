6904a42e84ef03df04cfb0ee0729621c0a1b3d52
Daiver <ra22341@ya.ru>
2013-05-10 13:06:06 +0400

    and so on

---
+++ cppexp/pcl/people_detect/CMakeFiles/ground_based_rgbd_people_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ground_based_rgbd_people_detector.dir/main.cpp.o"
+  "ground_based_rgbd_people_detector.pdb"
+  "ground_based_rgbd_people_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ground_based_rgbd_people_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
