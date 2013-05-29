61424d5a8677400da4186a835987c1d5ea285b79
Daiver <ra22341@ya.ru>
2013-05-29 10:04:44 +0400

    lkdemo added

---
+++ ocv/cpp/predator/CMakeFiles/lk2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lk2.dir/lkdemo.cpp.o"
+  "lk2.pdb"
+  "lk2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lk2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
