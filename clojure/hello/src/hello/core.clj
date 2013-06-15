063f9c7bd591b388d81e3fe9277dbb8360733418
Daiver <ra22341@ya.ru>
2013-06-15 20:48:41 +0400

    major sum

--- clojure/hello/src/hello/core.clj
+++ clojure/hello/src/hello/core.clj
@@ -1,51 +1,67 @@
 (ns hello.core)
 
-(defn square [x] (* x x))
 (defn abs [x] (if (< x 0) (- x) x))
-(defn good_enough? [guess x] (< (abs (- x (square guess))) 0.001))
-(defn good_enough2? [guess old_guess] (< (abs (- guess old_guess)) 0.001))
-
-(defn average 
-    [x y]
-    (/ (+ x y) 2.0)
-)
-
-(defn improve
-    [guess x]
-    (average guess (/ x guess))
-)
-
-(defn sqrt_iter
-    [guess x]
-    (if (good_enough? guess x)
-        guess
-        (sqrt_iter (improve guess x) x)
+(defn square [x] (* x x))
+(defn average [x y] (/ (+ x y) 2.0))
+(defn good_enough? [guess old_guess threshold] (< (abs (- guess old_guess)) threshold))
+(defn sum
+    ([a b term next]
+        (if (> a b)
+            0
+            (+ (term a)
+                (sum (next a) b term next)
+            )
+        )
     )
-)
 
-(defn sqrt_iter2
-    [guess old_guess x]
-    (if (good_enough2? guess old_guess)
-        guess
-        (sqrt_iter2 (improve guess x) guess x)
+    ([seq]
+        (if (= (count seq) 0)
+            0
+            (+ (first seq) (sum (next seq)))
+        )
     )
-)
 
-(defn useless_rec
-    []
-    (useless_rec)
-)
-
-(defn useless_param
-    [x]
-    100
+    ([]
+        0
+    )
 )
 
 (defn -main
     ""
     [& args]
     (println "version" (clojure-version))
-    (println (sqrt_iter2 1.0 900.0 900.0))
+    (println (sum 1 5 #(+ %1) #(+ %1 1)))
+    (println (sum [1 2 3 4 5]))
+    ;(println (cube_sqrt 75.))
+    ;(println (sqrt_iter2 1.0 900.0 900.0))
+)
+
+(comment
+    (defn good_enough? [guess x] (< (abs (- x (square guess))) 0.001))
+    (defn sqrt_iter
+        [guess x]
+        (if (good_enough? guess x)
+            guess
+            (sqrt_iter (improve guess x) x)
+        )
+    )
+    (defn useless_rec
+        []
+        (useless_rec)
+    )
+
+    (defn useless_param
+        [x]
+        100
+    )
+
+    (defn find_brackets
+        [l s]
+        (if (> (count s) 0)
+            (concat l [(first s)] (find_brackets l (next s)))
+            ""
+        )
+    )
     ;(println (sqrt_iter 1.0 900.0))
     ;(println (useless_param (useless_rec)))
     ;(println (abs -10))
@@ -80,13 +96,34 @@
         )
         (.substring s (.indexOf s "(") (+ (.indexOf s ")") 1) )
     )
+(defn improve [guess x] (average guess (/ x guess)))
+
+(defn good_enough2? [guess old_guess] (< (abs (- guess old_guess)) 0.001))
+(defn sqrt_iter2
+    [guess old_guess x]
+    (if (good_enough2? guess old_guess)
+        guess
+        (sqrt_iter2 (improve guess x) guess x)
+    )
+)
+(defn cube_improve
+    [guess x]
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3)
 )
 
-(defn find_brackets
-    [l s]
-    (if (> (count s) 0)
-        (concat l [(first s)] (find_brackets l (next s)))
-        ""
+(defn cube_sqrt_iter
+    [guess old_guess x]
+    (if (good_enough? guess old_guess 0.001)
+        guess
+        (cube_sqrt_iter (cube_improve guess x) guess x)
     )
+)
+
+(defn cube_sqrt
+    [x]
+    (cube_sqrt_iter 1. x x)
+)
+
+
 )
 
