f203ad54494a34f77d4ac70ef9650d9e5a2cc4e4
Daiver <ra22341@ya.ru>
2013-06-23 19:12:01 +0400

    add minor openmp sample

---
+++ cppexp/openmp/hello1/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <omp.h>
+ 
+#define N 100
+ 
+int main(int argc, char *argv[])
+{
+  double a[N], b[N], c[N];
+  int i;
+  omp_set_dynamic(0);      // запретить библиотеке openmp менять число потоков во время исполнения
+  omp_set_num_threads(10); // установить число потоков в 10
+ 
+  // инициализируем массивы
+  for (i = 0; i < N; i++)
+  {
+      a[i] = i * 1.0;
+      b[i] = i * 2.0;
+  }
+ 
+  // вычисляем сумму массивов
+#pragma omp parallel shared(a, b, c) private(i)
+  {
+#pragma omp for
+    for (i = 0; i < N; i++)
+      c[i] = a[i] + b[i];
+  }
+  for(i =  0; i < 11; i++)
+  printf ("%f\n", c[i]);
+  return 0;
+}
