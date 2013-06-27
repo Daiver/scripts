bb305bed6967a2aa98fe7e3489c6dd3d18c54ffb
Daiver <ra22341@ya.ru>
2013-06-27 12:14:23 +0400

    123

--- clojure/classifier/src/classifier/core.clj
+++ clojure/classifier/src/classifier/core.clj
@@ -3,8 +3,8 @@
 (use 'clojure.java.io)
 
 (defn read_data_from_file [fname]
-    (map #(split % #"\s") (with-open [rdr (reader fname)] 
-        (doall (line-seq rdr)))))
+    (map #(split % #"\s")(map lower-case (with-open [rdr (reader fname)] 
+        (doall (line-seq rdr))))))
 
 (defn do-to-map [amap keyseq f]
     (reduce #(assoc %1 %2 (f (%1 %2))) amap keyseq))
@@ -13,7 +13,7 @@
     (let [data (group-by #(first %) raw_data)]
         (do-to-map
             data (keys data) 
-                (fn [x] (reduce concat (map #(rest %) x))))))
+                (fn [x] (frequencies (reduce concat (map #(rest %) x)))))))
         
 
 (defn -main
@@ -21,6 +21,11 @@
     [& args]
     (println args)
     (def raw_data (read_data_from_file (first args)))
-
-    (println (dicts_from_data raw_data))
+    (def d (group-by #(first %) raw_data))
+    ;(def d1 (map #(reduce concat (d %)) (keys d)))
+    (def f (map frequencies raw_data))
+    (def d1 (apply concat (d "spam")))
+    (println (apply concat (d "ham")))
+    ;(println (dicts_from_data raw_data))
+    (println "end")
 )
